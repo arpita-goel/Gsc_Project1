2dac1036d871fe18ba0d76e19193cc44503906c4
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-11-08 15:58:34 +0530

    Update API URL and modify back button functionality

--- lib/pages/set_reminder.dart
+++ lib/pages/set_reminder.dart
@@ -14,7 +14,7 @@ Future<void> saveReminderToBackend({
   try {
     final response = await http.post(
       Uri.parse(
-          'https://zindagigo.onrender.com/api/reminders'), // Replace with your backend URL
+          'https://zindagigo-1gr2.onrender.com/api/reminders'), // Replace with your backend URL
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
         'title': title,
@@ -98,13 +98,18 @@ class _SetReminderPageState extends State<SetReminderPage> {
               children: [
                 Align(
                   alignment: Alignment.centerLeft,
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      size: 28,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context); // Go back to previous page
+                  child: Builder(
+                    builder: (context) {
+                      return IconButton(
+                        icon: Image.asset(
+                          'lib/imagesOrlogo/Drawer.png',
+                          width: 50,
+                          height: 23,
+                        ),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      );
                     },
                   ),
                 ),
@@ -123,6 +128,184 @@ class _SetReminderPageState extends State<SetReminderPage> {
           ),
         ),
       ),
+      // drawer: Drawer(
+      //   backgroundColor: AppColors.drawerColor,
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: [
+      //       // Drawer Header
+      //       DrawerHeader(
+      //         //decoration: BoxDecoration(color: AppColors.drawerColor),
+      //         decoration: BoxDecoration(
+      //           color: AppColors.drawerColor, // Make sure it blends with the drawer background
+      //         ),
+      //         child: Row(
+      //           children: [
+      //             SizedBox(
+      //               width: 40,
+      //               height: 40,
+      //               child: const Icon(
+      //                 Icons.favorite,
+      //                 color: AppColors.pink,
+      //                 size: 50,
+      //               ),
+      //             ),
+      //             const SizedBox(width: 10),
+      //             const Text(
+      //               "HELLO USER!!",
+      //               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      //             ),
+      //             const Spacer(),
+      //             // IconButton(
+      //             //   icon: const Icon(Icons.close),
+      //             //   onPressed: () {
+      //             //     Navigator.pop(context);
+      //             //   },
+      //             // ),
+      //           ],
+      //         ),
+      //       ),
+
+      //       // Search Bar
+      //       Padding(
+      //         padding: const EdgeInsets.symmetric(horizontal: 10),
+      //         child: Container(
+      //           decoration: BoxDecoration(
+      //             color: AppColors.searchBar, // Background color
+      //             borderRadius: BorderRadius.circular(20), // Rounded corners
+      //             boxShadow: const [
+      //               BoxShadow(
+      //                 color: Colors.black12,
+      //                 blurRadius: 5,
+      //                 spreadRadius: 1,
+      //               ),
+      //             ],
+      //           ),
+      //           child: TextField(
+      //             decoration: InputDecoration(
+      //               hintText: "search...",
+      //               prefixIcon: const Icon(Icons.search, color: Colors.black),
+      //               border: InputBorder.none,
+      //               contentPadding: const EdgeInsets.symmetric(vertical: 10),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+
+      //       const SizedBox(height: 10),
+
+      //       // Menu Items
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/home2.png',
+      //           width: 30,
+      //           height: 27,
+      //         ),
+      //         title: const Text("Home"),
+      //         onTap: () {
+      //           Navigator.pop(context);
+      //           Navigator.pushNamed(context, '/home');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/settings.png',
+      //           width: 27,
+      //           height: 27,
+      //         ),
+      //         title: const Text("Settings"),
+      //         onTap: () {
+      //           Navigator.pushNamed(context, '/settingspage');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/Notification.png',
+      //           width: 31,
+      //           height: 32,
+      //         ),
+      //         title: const Text("Notifications"),
+      //         onTap: () {
+      //           Navigator.pushNamed(context, '/notificationpage');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/phone.png',
+      //           width: 31,
+      //           height: 32,
+      //         ),
+      //         title: const Text("Help Numbers"),
+      //         onTap: () {
+      //           Navigator.pushNamed(context, '/helpNumbers');
+      //         },
+      //       ),            
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/profile.png',
+      //           width: 25,
+      //           height: 26,
+      //         ),
+      //         title: const Text("Profile"),
+      //         onTap: () {
+      //           Navigator.pushNamed(context, '/profilepage');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Image.asset(
+      //           'lib/imagesOrlogo/Logout.png',
+      //           width: 35,
+      //           height: 36,
+      //         ),
+      //         title: const Text("Logout"),
+      //         onTap: () {
+      //           // logout(context);
+      //         },
+      //       ),
+
+      //       const Spacer(),
+
+      //       // Light & Dark Mode Toggle
+      //       Padding(
+      //         padding: const EdgeInsets.all(10),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             ElevatedButton.icon(
+      //               onPressed: () {
+      //                 // Light mode action
+      //               },
+      //               icon: const Icon(Icons.light_mode, color: Colors.black),
+      //               label: const Text("Light"),
+      //               style: ElevatedButton.styleFrom(
+      //                 backgroundColor: AppColors.searchBar,
+      //                 foregroundColor: Colors.black,
+      //                 shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.circular(20),
+      //                 ),
+      //               ),
+      //             ),
+      //             const SizedBox(width: 10),
+      //             ElevatedButton.icon(
+      //               onPressed: () {
+      //                 // Dark mode action
+      //               },
+      //               icon: const Icon(Icons.dark_mode, color: Colors.white),
+      //               label: const Text("Dark"),
+      //               style: ElevatedButton.styleFrom(
+      //                 backgroundColor: Colors.black,
+      //                 foregroundColor: Colors.white,
+      //                 shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.circular(20),
+      //                 ),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 2.0),
