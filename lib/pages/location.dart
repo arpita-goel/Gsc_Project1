2f3a86bd84a2a61fe60dd665d8b6c5d369960595
arpita-goel <148207309+arpita-goel@users.noreply.github.com>
2025-11-08 16:05:50 +0530

    Comment out code in location.dart
    Comment out imports and class methods in LocationPage.

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,85 +1,85 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-class LocationPage extends StatefulWidget {
-  const LocationPage({super.key});
+// class LocationPage extends StatefulWidget {
+//   const LocationPage({super.key});
 
-  @override
-  State<LocationPage> createState() => _LocationPageState();
-}
+//   @override
+//   State<LocationPage> createState() => _LocationPageState();
+// }
 
-class _LocationPageState extends State<LocationPage> {
-  String _status = 'Fetching location...';
+// class _LocationPageState extends State<LocationPage> {
+//   String _status = 'Fetching location...';
 
-  @override
-  void initState() {
-    super.initState();
-    _openMapsWithCurrentLocation();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _openMapsWithCurrentLocation();
+//   }
 
  
-Future<void> _openMapsWithCurrentLocation() async {
-  bool serviceEnabled;
-  LocationPermission permission;
+// Future<void> _openMapsWithCurrentLocation() async {
+//   bool serviceEnabled;
+//   LocationPermission permission;
 
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    setState(() {
-      _status = 'Location services are disabled.';
-    });
-    return;
-  }
+//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//   if (!serviceEnabled) {
+//     setState(() {
+//       _status = 'Location services are disabled.';
+//     });
+//     return;
+//   }
 
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      setState(() {
-        _status = 'Location permissions are denied';
-      });
-      return;
-    }
-  }
+//   permission = await Geolocator.checkPermission();
+//   if (permission == LocationPermission.denied) {
+//     permission = await Geolocator.requestPermission();
+//     if (permission == LocationPermission.denied) {
+//       setState(() {
+//         _status = 'Location permissions are denied';
+//       });
+//       return;
+//     }
+//   }
 
-  if (permission == LocationPermission.deniedForever) {
-    setState(() {
-      _status = 'Location permissions are permanently denied.';
-    });
-    return;
-  }
+//   if (permission == LocationPermission.deniedForever) {
+//     setState(() {
+//       _status = 'Location permissions are permanently denied.';
+//     });
+//     return;
+//   }
 
-  Position position = await Geolocator.getCurrentPosition(
-    desiredAccuracy: LocationAccuracy.high,
-  );
+//   Position position = await Geolocator.getCurrentPosition(
+//     desiredAccuracy: LocationAccuracy.high,
+//   );
 
-  final latitude = position.latitude;
-  final longitude = position.longitude;
+//   final latitude = position.latitude;
+//   final longitude = position.longitude;
 
-final Uri geoUri = Uri.parse('geo:$latitude,$longitude?q=nearby+places');
+// final Uri geoUri = Uri.parse('geo:$latitude,$longitude?q=nearby+places');
 
-if (await canLaunchUrl(geoUri)) {
- await launchUrl(geoUri, mode: LaunchMode.externalApplication);
-} else {
-  setState(() {
-    _status = 'Could not open Google Maps.';
-  });
-}
+// if (await canLaunchUrl(geoUri)) {
+//  await launchUrl(geoUri, mode: LaunchMode.externalApplication);
+// } else {
+//   setState(() {
+//     _status = 'Could not open Google Maps.';
+//   });
+// }
 
-}
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Location'),
-      ),
-      body: Center(
-        child: Text(
-          _status,
-          style: const TextStyle(fontSize: 18),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Location'),
+//       ),
+//       body: Center(
+//         child: Text(
+//           _status,
+//           style: const TextStyle(fontSize: 18),
+//         ),
+//       ),
+//     );
+//   }
+// }
